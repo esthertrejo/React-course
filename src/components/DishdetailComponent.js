cebe2d66f159788e07bd270c90d55d04768c1426
esthertrejo <67111630+esthertrejo@users.noreply.github.com>
2020-09-07 17:37:56 -0500

    Presentational and Container Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class Dishdetail extends Component {
     constructor(props) {
@@ -27,7 +27,7 @@ class Dishdetail extends Component {
             return(
                 <div key = {c.id}>
                     <li className="m-2">{ c.comment }</li>
-                    <li>{ '-- ' + c.author }</li>
+                    <li>{ '-- ' + c.author + ', ' + new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(c.date))) }</li>
                 </div>
             )
         });
@@ -36,19 +36,21 @@ class Dishdetail extends Component {
     }
 
     render() {
-        const dish = this.props.selectedDish;
+        const dish = this.props.dish;
 
         if(dish != null){
             return (
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        { this.renderDish(dish) }
-                    </div>
-                    <div className="col-12 col-md-5 m-1">
-                        <h4>Comments</h4>
-                        <ul className="list-unstyled">
-                            { this.renderComments(dish.comments) }
-                        </ul>
+                <div className="container">                
+                    <div className="row">
+                        <div className="col-12 col-md-5 m-1">
+                            { this.renderDish(dish) }
+                        </div>
+                        <div className="col-12 col-md-5 m-1">
+                            <h4>Comments</h4>
+                            <ul className="list-unstyled">
+                                { this.renderComments(dish.comments) }
+                            </ul>
+                        </div>
                     </div>
                 </div>
             );
