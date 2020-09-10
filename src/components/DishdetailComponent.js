b388ed3b9ee41b1fde6ea2ae4ba2584546158c9f
esthertrejo <67111630+esthertrejo@users.noreply.github.com>
2020-09-10 15:36:03 -0500

    Single Page Applications Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
     function RenderDish({dish}) {
         if(dish != null){
@@ -31,17 +32,30 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
     }
 
     const Dishdetail = (props) => {
-        const dish = props.dish;
 
-        if(dish != null){
+        if(props.dish != null){
             return (
-                <div className="container">                
+                <div className="container">   
                     <div className="row">
-                        <RenderDish dish={dish} />
+                        <Breadcrumb>
+                            <BreadcrumbItem>
+                                <Link to="/menu">Menu</Link>
+                            </BreadcrumbItem>
+                            <BreadcrumbItem active>
+                                {props.dish.name}
+                            </BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>{props.dish.name}</h3>
+                            <hr />
+                        </div>
+                    </div>   
+                    <div className="row">
+                        <RenderDish dish={props.dish} />
                         <div className="col-12 col-md-5 m-1">
                             <h4>Comments</h4>
                             <ul className="list-unstyled">
-                                <RenderComments comments= { dish.comments } />
+                                <RenderComments comments= { props.comments } />
                             </ul>
                         </div>
                     </div>
